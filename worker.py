d81182a4524cd5cb11d5a39a0ccd10c4d1826987
rubensander <rubensander@gmx.de>
2025-11-05 12:43:17 +0100

    platform & logging

--- worker.py
+++ worker.py
@@ -1,34 +1,40 @@
+# -*- coding: utf-8 -*-
 import requests
 import mysql.connector
 from datetime import datetime as dt
 from time import sleep
 import sys
+import logging
 
 DB_HOST = 'localhost'
-DB_USER = 'kruppallee'
+DB_USER = 'root'
 DB_DATABASE = 'kruppallee'
-DB_PASSWORD = 'auiasd6fa7h2j23%/bwwer'  # Replace with your actual password
+DB_PASSWORD = '0000'  # Replace with your actual password
 API_URL = "https://ifa.ruhrbahn.de/departure/20009409" #9409
 
-def store_data(datetime, line, realdatetime=None, delay=None, realtime=None, direction=None):
+def store_data(datetime, line, platform, realdatetime=None, delay=None, realtime=None, direction=None):
     # Connect to MySQL database
     connection = mysql.connector.connect(host=DB_HOST, user=DB_USER, database=DB_DATABASE, password=DB_PASSWORD)
     cursor = connection.cursor()
 
     # Query to insert data into the database
     insert_query = """
-        REPLACE INTO departures (datetime, line, realdatetime, delay, realtime, direction)
-        VALUES (%s, %s, %s, %s, %s, %s)
+        REPLACE INTO departures (datetime, line, realdatetime, delay, realtime, direction, platform)
+        VALUES (%s, %s, %s, %s, %s, %s, %s)
     """
 
-    data_tuple = (datetime, line, realdatetime, delay, realtime, direction)
+    data_tuple = (datetime, line, realdatetime, delay, realtime, direction, platform)
     cursor.execute(insert_query, data_tuple)
     connection.commit()
     cursor.close()
     connection.close()
-    print("Data stored successfully.")
+
+    logging.info(f"Stored data: {data_tuple}")
 
 def main():
+    logging.basicConfig(level=logging.WARN, format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.getLogger().addHandler(logging.FileHandler("log.txt"))
+
     response = requests.get(API_URL)
 
     # Statuscode prüfen
@@ -36,10 +42,11 @@ def main():
         data = response.json()  # Antwort als JSON lesen
         departure_list = data["data"]["departureList"]
 
-        for departure in departure_list:
+        if not departure_list:
+            logging.warning("No departures found in the response.")
+            return
 
-            if departure.get("platform") != "1":
-                continue
+        for departure in departure_list:
 
             datetime = parse_datetime(departure["dateTime"])
             line = departure["servingLine"].get("symbol")
@@ -47,13 +54,14 @@ def main():
             delay = departure["servingLine"].get("delay")
             realtime = departure["servingLine"].get("realtime")
             direction = departure["servingLine"].get("direction")
+            platform = departure.get("platform")
 
             if "realDateTime" in departure:
                 realdatetime = parse_datetime(departure["realDateTime"])
             
-            store_data(datetime, line, realdatetime, delay, realtime, direction)
+            store_data(datetime, line, platform, realdatetime, delay, realtime, direction)
     else:
-        print("Fehler:", response.status_code)
+        logging.error(response.status_code)
 
 def parse_datetime(date_dict):
     keys = ['year', 'month', 'day', 'hour', 'minute']
